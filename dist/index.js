63be96d9287c9d77a660069e576908aa0d8657f7
Rudi <rudi.memachem@sunbit.com>
2023-09-06 14:17:53 +0300

    Use vercel/ncc

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// Create a variable to store the result of the sum
+
+var sum = 1 + 1;
+
+// Print the result to the console
+
+console.log("The sum of 1 + 1 is: " + sum);
+
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
